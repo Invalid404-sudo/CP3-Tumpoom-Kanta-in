c42845673d3159e1ebe8d2fe26e6e3c3dfd11845
Invalid404-sudo <72086542+Invalid404-sudo@users.noreply.github.com>
2021-01-10 23:48:55 +0900

    Create Exercise4_Tumpoom_K.py

---
+++ Exercise4_Tumpoom_K.py
@@ -0,0 +1,10 @@
+FE =80
+GB =85
+ICP =70
+CP =69
+
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICP)
+print("Computer Programming :", CP)
