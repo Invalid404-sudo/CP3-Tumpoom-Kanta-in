a520c0afbc35878eefc9ebb9cab436a368f90d8b
Invalid404-sudo <72086542+Invalid404-sudo@users.noreply.github.com>
2021-01-12 15:30:49 +0900

    Create Lecture50_Tumpoom_K.py

---
+++ Lecture50_Tumpoom_K.py
@@ -0,0 +1,12 @@
+def addNumber(X,Y):
+    print(X+Y)
+def minusNumber(X,Y):
+    print(X-Y)
+def multiply(X,Y):
+    print(X*Y)
+def divide(X,Y):
+    print(X/Y)
+addNumber(10,20)
+minusNumber(20,10)
+multiply(20,10)
+divide(20,10)
