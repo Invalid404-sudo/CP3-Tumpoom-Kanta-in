a0484180d263bcde768c7830e06bc279d0f499e1
Invalid404-sudo <72086542+Invalid404-sudo@users.noreply.github.com>
2021-01-15 17:26:00 +0900

    Create Lecture104_Tumpoom_K.py

---
+++ Lecture104_Tumpoom_K.py
@@ -0,0 +1,25 @@
+class Customer:
+    name = ""
+    lastName = ""
+    age = 0
+    def addCart(X) :
+        print("Add to Cart",X.name,X.lastName+"'s Cart")
+Customer1 = Customer()
+Customer1.name = "Tumpoom"
+Customer1.lastName = "Kanta-in"
+Customer1.addCart()
+
+Customer2 = Customer()
+Customer2.name = "June"
+Customer2.lastName = "Supata"
+Customer2.addCart()
+
+Customer3= Customer()
+Customer3.name = "Com"
+Customer3.lastName = "Gaming"
+Customer3.addCart()
+
+Customer4= Customer()
+Customer4.name = "Fluke"
+Customer4.lastName = "Dan"
+Customer4.addCart()
